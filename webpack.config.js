848c5ba543624375601125e8b5eb2fec94a322f5
mathias73 <mat.micheli99@gmail.com>
2021-01-21 16:07:16 +0100

    first home page

--- webpack.config.js
+++ webpack.config.js
@@ -70,8 +70,7 @@ Encore
     // requires WebpackEncoreBundle 1.4 or higher
     //.enableIntegrityHashes(Encore.isProduction())
 
-    // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    .autoProvidejQuery()
 
     .enablePostCssLoader()
 
