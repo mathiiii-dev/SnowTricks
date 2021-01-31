894cd06cbfbdaa41b0969bb0c1ba19f1f238c217
Unknown <mat.micheli99@gmail.com>
2021-01-31 17:04:18 +0100

    delete some useless files

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,4 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
-// start the Stimulus application
-import './bootstrap';
-
-import '../templates/home/home';
+import '../js/home/home';
