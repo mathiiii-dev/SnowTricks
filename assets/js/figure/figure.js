d7c2b338be00fa072fd48808ffd8800ac415bd2e
Unknown <mat.micheli99@gmail.com>
2021-02-20 21:19:49 +0100

    correct bug delete in figure page

--- assets/js/figure/figure.js
+++ assets/js/figure/figure.js
@@ -4,7 +4,7 @@ let hiddenModal = document.getElementById('hidden-modal');
 deleteFigure.addEventListener('click', (e) => {
     document.getElementById('figure-name').innerHTML = deleteFigure.dataset.name;
     hiddenModal.classList.remove('hidden');
-    document.getElementById('delete-action').setAttribute('href', 'delete-figure/' + deleteFigure.dataset.id);
+    document.getElementById('delete-action').setAttribute('href', deleteFigure.dataset.url);
 })
 
 hiddenModal.addEventListener('click', (e) => {
